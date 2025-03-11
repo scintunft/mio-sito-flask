0055debc8e1b1c397afc4b7ee05873ce71b15d07
manuel <scintunft@gmail.com>
2025-03-11 04:09:44 +0100

    Sposto index.html nella cartella templates

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Sito Flask online!"
+    return render_template("index.html")  # Renderizza il file index.html
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Usa la porta di Render, altrimenti 5000
+    port = int(os.environ.get("PORT", 10000))  # Usa la porta fornita da Render
     app.run(host="0.0.0.0", port=port)
