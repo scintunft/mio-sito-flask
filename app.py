0edd3aa25783e6f648141e36d9f7c32f5bba3532
manuel <scintunft@gmail.com>
2025-03-11 04:55:48 +0100

    Modificato app.py per correggere il problema

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 import os
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")  # Specifica la cartella dei template
 
 @app.route("/")
 def home():
-    return render_template("index.html")  # Renderizza il file index.html
+    return render_template("index.html")  
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))  # Usa la porta fornita da Render
+    port = int(os.environ.get("PORT", 10000))  
     app.run(host="0.0.0.0", port=port)
