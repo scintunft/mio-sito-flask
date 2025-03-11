63df1536ba89aa1b01924d6bf1e4c98fcc718362
manuel <scintunft@gmail.com>
2025-03-11 03:17:27 +0100

    Fix porta per Render

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template
+import os
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
-    return render_template('index.html')
+    return "Sito Flask online!"
 
-if __name__ == '__main__':
-    app.run(debug=True)
-
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Usa la porta di Render, altrimenti 5000
+    app.run(host="0.0.0.0", port=port)
